ef8583668e10f80154dd8394c6ce289d5752c474
Andrey Belyak <beliak@mail.ru>
2014-04-27 22:11:42 +0400

    добавлена фция формирования ответа сервера

---
+++ create_message.cpp
@@ -0,0 +1,13 @@
+#include "create_message.h"
+
+#include "lgs_constants.h"
+
+#include <string.h>
+#include <stdio.h>
+
+char * create_message(const int code, const char * msg) {
+    static char buff[MAX_LINE_LENGTH];
+    memset(buff, 0, sizeof(buff));
+    sprintf(buff, "%d %s%s", code, msg, LINES_SEPARATOR);
+    return buff;
+}
---
+++ create_message.h
@@ -0,0 +1,7 @@
+#ifndef CREATE_MESSAGE_H
+#define	CREATE_MESSAGE_H
+
+char * create_message(const int code, const char * msg);
+
+#endif	/* CREATE_MESSAGE_H */
+
