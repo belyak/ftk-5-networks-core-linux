f37d4c3f20e051003deb61c2ec0d2a5d9725ef6c
Andrey Belyak <beliak@mail.ru>
2014-05-09 17:55:03 +0400

    fix

--- lgs_constants.h
+++ lgs_constants.h
@@ -2,7 +2,7 @@
 #define LGS_CONSTANTS_H_
 
 #define SERVER_BANNER_P1 L"Text frequency analysis server v."
-#define SERVER_BANNER_P2 L"ready."
+#define SERVER_BANNER_P2 L" ready."
 
 #define SERVER_VERSION L"0.02"
 
